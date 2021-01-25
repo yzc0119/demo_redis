ab68bc19fb53759aea867170c027d5e4b5822b61
yang-zc <861478861@qq.com>
2021-01-25 16:43:20 +0800

    masters

--- src/main/java/com/offcn/controller/DemoController.java
+++ src/main/java/com/offcn/controller/DemoController.java
@@ -2,16 +2,14 @@ package com.offcn.controller;
 
 public class DemoController {
 
-    public  void  demo(){
+    public void demo() {
 
         System.out.println("this is master demo");
     }
 
-    public void test(){
+    public void test() {
         System.out.println("this is  master test");
     }
 
-    public  void  demo(){
-        System.out.println("这个是分支的demo");
-    }
 }
+
