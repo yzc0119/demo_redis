5974c0363c567096a2afb11baffaf82633b77fbb
yang-zc <861478861@qq.com>
2021-01-25 16:17:58 +0800

    master

---
+++ src/main/java/com/offcn/controller/DemoController.java
@@ -0,0 +1,13 @@
+package com.offcn.controller;
+
+public class DemoController {
+
+    public  void  demo(){
+
+        System.out.println("this is master demo");
+    }
+
+    public void test(){
+        System.out.println("this is  master test");
+    }
+}
