f49c6858094f9b879b7b0e4d2927d8fd2b1c5e0f
yang-zc <861478861@qq.com>
2021-01-25 16:37:21 +0800

    yzc

--- src/main/java/com/offcn/controller/DemoController.java
+++ src/main/java/com/offcn/controller/DemoController.java
@@ -6,4 +6,8 @@ public class DemoController {
     public  void test(){
         System.out.println("这个是分支中的内容");
     }
+
+    public  void  demo(){
+        System.out.println("这个是分支的demo");
+    }
 }
